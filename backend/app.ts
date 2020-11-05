dc7455eb753fb8f1b01a562d7961e8f3491cafb4
Sylvain Mathieu <sylvain.mathieu@gmail.com>
2020-11-05 07:19:59 +0000

    Bootstrapped backend

---
+++ backend/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from "express"
+import * as http from "http"
+
+const app: Application = express()
+const server: http.Server = http.createServer(app)
+const port = 3000
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send(`Server running at port ${port}`)
+})
+
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`)
+})
