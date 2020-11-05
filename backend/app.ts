eddb69b87619184d2fd76659603a7ec1c7574b91
Sylvain Mathieu <sylvain.mathieu@gmail.com>
2020-11-05 17:37:20 +0000

    Handle HTTPS in dev mode

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,24 @@
 import express, { Application, Request, Response } from "express"
 import * as http from "http"
+import * as https from "https"
+import * as fs from "fs"
 
 const app: Application = express()
-const server: http.Server = http.createServer(app)
-const port = 3000
+
+let server: https.Server | http.Server
+let port: number
+if (process.env.MODE != "production") {
+    const httpsOptions = {
+        cert: fs.readFileSync(__dirname + "/../certs/shopify-app.dev.crt"),
+        key: fs.readFileSync(__dirname + "/../certs/shopify-app.dev.key")
+    }
+    server = https.createServer(httpsOptions, app)
+    port = 443
+} else {
+    // In prod, SSL will be dealt with by ngnix
+    server = http.createServer(app)
+    port = 80
+}
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).send(`Server running at port ${port}`)
