8539fefb463fabe3fe665b57f35d0585d93212f4
Sylvain Mathieu <sylvain.mathieu@gmail.com>
2020-11-08 15:33:50 +0000

    Added models

--- backend/database/000_baseline.sql
+++ backend/database/000_baseline.sql
@@ -32,7 +32,7 @@ create table shop_resources(
 create unique index ind_unique_shop_resource_gid on shop_resources (resource_type, resource_id);
 create index ind_shop_resource_shop_id on shop_resources (shop_id);
 
-create table product_capacities(
+create table resource_capacities(
 	id uuid default uuid_generate_v4(),
 	shop_resource_id uuid not null references shop_resources(id),
 	capacity integer,
@@ -43,6 +43,6 @@ create table product_capacities(
 	primary key (id)
 );
 
-create index ind_product_capacities_shop_resource_id on product_capacities (shop_resource_id);
-create index ind_product_capacities_start_date on product_capacities (start_date);
-create index ind_product_capacities_end_date on product_capacities (end_date);
+create index ind_resource_capacities_shop_resource_id on resource_capacities (shop_resource_id);
+create index ind_resource_capacities_start_date on resource_capacities (start_date);
+create index ind_resource_capacities_end_date on resource_capacities (end_date);
---
+++ backend/src/shopResource/shopResource.model.ts
@@ -0,0 +1,28 @@
+interface ShopResourceSchema {
+	id: string
+	shop_id: string
+	resource_type: string
+	resource_id: string
+	title: string
+	created_date?: Date
+}
+
+interface ShopResource {
+	id: string
+	shopId: string
+	resourceType: string
+	resourceId: string
+	title: string
+	createdDate?: Date
+}
+
+export function toShopResource(schema: ShopResourceSchema): ShopResource {
+	return {
+		id: schema.id,
+		shopId: schema.shop_id,
+		resourceType: schema.resource_type,
+		resourceId: schema.resource_id,
+		title: schema.title,
+		createdDate: schema.created_date
+	}
+}
