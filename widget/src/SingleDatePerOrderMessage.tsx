2e264a6cab5ea050077f435e0dfef3e7e1c86772
Sylvain Mathieu <sylvain.mathieu@gmail.com>
2021-08-02 21:48:21 +0100

    Makes it possible to hide the “single date per order” message

--- widget/src/SingleDatePerOrderMessage.tsx
+++ widget/src/SingleDatePerOrderMessage.tsx
@@ -7,6 +7,6 @@ interface Props {
 }
 
 export default function SingleDatePerOrderMessage({ settings }: Props) {
-	const singleDatePerOrderMessage = settings.messages.singleDatePerOrderMessage || DEFAULT_SINGLE_DATE_PER_ORDER_MESSAGE
-	return <div className="buunto-info-message">{singleDatePerOrderMessage}</div>
+	const singleDatePerOrderMessage = settings.messages.singleDatePerOrderMessage ?? DEFAULT_SINGLE_DATE_PER_ORDER_MESSAGE
+	return singleDatePerOrderMessage ? <div className="buunto-info-message">{singleDatePerOrderMessage}</div> : undefined
 }
